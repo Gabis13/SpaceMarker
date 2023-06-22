a567893525ac45e92799824368cfce5e181cb463
Gabriel Viecili <gabriel.viecili@gmail.com>
2023-06-22 19:45:21 -0300

    Base funcionando

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import winsound
-
+from tkinter import simpledialog
 
 
 pygame.init()
@@ -10,8 +10,10 @@ screen =  pygame.display.set_mode( scale )
 pygame.display.set_caption("SpaceMarker")
 running = True
 sky = pygame.image.load("bg.jpg")
-spaceIco = pygame.image.load("space.ico")
+spaceIco = pygame.image.load("space.png")
 space = pygame.image.load("space.png")
+star = []
+
 
 pygame.display.set_icon(spaceIco)
 
@@ -20,6 +22,16 @@ while running:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             running = False
+        elif evento.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space", "Nome da Estrela:" )
+            print(item)
+            if item == None:
+                item = "desconhecido"+str(pos)
+            star[item] = pos
+
+    screen.fill(white)
+    screen.blit( sky, (0,0) )
 
     pygame.display.update()
 pygame.quit()
